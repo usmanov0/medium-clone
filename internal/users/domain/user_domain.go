8cc7c342f66c617a1524884c67a4ebe716b90427
usmanov0 <azikusmanov882@gmail.com>
2023-12-27 23:28:49 +0500

    created :domain

---
+++ internal/users/domain/user_domain.go
@@ -0,0 +1,20 @@
+package domain
+
+import "time"
+
+type User struct {
+	Id        int
+	UserName  string
+	Email     string
+	Password  string
+	Bio       string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type NewUser struct {
+	UserName  string
+	Email     string
+	Password  string
+	CreatedAt time.Time
+}
