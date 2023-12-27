3a843bc4ccb452b4330f340253a8b14c7a87e8ed
usmanov0 <azikusmanov882@gmail.com>
2023-12-27 23:02:24 +0500

    user factory

---
+++ internal/entity/user/entity/user_factory.go
@@ -0,0 +1,18 @@
+package entity
+
+import "time"
+
+type UserFactory struct{}
+
+func NewUserFactory() *UserFactory {
+	return &UserFactory{}
+}
+
+func (f UserFactory) CreateNewUser(user *NewUser) *User {
+	return &User{
+		UserName:  user.UserName,
+		Email:     user.Email,
+		Password:  user.Password,
+		CreatedAt: time.Now().UTC(),
+	}
+}
