48afacc799a6ef4bc4b14a4e7994f8377845fa2c
usmanov0 <azikusmanov882@gmail.com>
2023-12-28 15:04:30 +0500

    added :errors

--- internal/users/errors/errors.go
+++ internal/users/errors/errors.go
@@ -1,12 +1,16 @@
 package errors
 
 const (
-	ErrUserNotFound       = Err("User not found")
-	ErrInvalidPassword    = Err("Invalid password")
-	ErrEmptyUserName      = Err("User name is empty")
-	ErrEmptyMail          = Err("Email address is empty")
-	ErrInvalidEmailFormat = Err("Invalid email format")
-	ErrBadCredentials     = Err("Bad credentials")
+	ErrUserNotFound        = Err("User not found")
+	ErrUserUpdateFailed    = Err("Failed to update user")
+	ErrInvalidPassword     = Err("Invalid password")
+	ErrEmptyUserName       = Err("User name is empty")
+	ErrEmptyMail           = Err("Email address is empty")
+	ErrInvalidEmailFormat  = Err("Invalid email format")
+	ErrBadCredentials      = Err("Bad credentials")
+	ErrIdScanFailed        = Err("Failed to scan id")
+	ErrFailedDeleteAccount = Err("Failed to delete user account")
+	ErrDeletingUser        = Err("Failed to delete user")
 )
 
 type Err string
