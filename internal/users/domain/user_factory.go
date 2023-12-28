f4cff6e2b8486eac4719231f6af61dfed8cffcb9
usmanov0 <azikusmanov882@gmail.com>
2023-12-28 15:45:18 +0500

    validations transferred to factory

--- internal/users/domain/user_factory.go
+++ internal/users/domain/user_factory.go
@@ -1,6 +1,11 @@
 package domain
 
-import "time"
+import (
+	"example.com/my-medium-clone/internal/users/errors"
+	"regexp"
+	"strings"
+	"time"
+)
 
 type UserFactory struct{}
 
@@ -15,4 +20,55 @@ func (f UserFactory) CreateNewUser(user *NewUser) *User {
 		Password:  user.Password,
 		CreatedAt: time.Now().UTC(),
 	}
+}
+
+func ValidateUserInfoForSignUp(userName, email, password string) error {
+
+	if strings.TrimSpace(userName) == "" {
+		return errors.ErrEmptyUserName
+	}
+	if strings.TrimSpace(email) == "" {
+		return errors.ErrEmptyMail
+	}
+	if validatePassword(password) == errors.ErrInvalidPassword {
+		return errors.ErrInvalidPassword
+	}
+	return nil
+}
+
+func ValidateUserInfoForSignIn(email, password string) error {
+	if strings.TrimSpace(email) == "" {
+		return errors.ErrBadCredentials
+	}
+	if validatePassword(password) == errors.ErrInvalidPassword {
+		return errors.ErrBadCredentials
+	}
+	return nil
+}
+
+func validatePassword(password string) error {
+	if len(password) < 8 {
+		return errors.ErrInvalidPassword
+	}
+
+	var (
+		upperCase = regexp.MustCompile(`[A-Z]`)
+		lowerCase = regexp.MustCompile("`[a-z]`")
+		digit     = regexp.MustCompile(`[0-9]`)
+	)
+	if !upperCase.MatchString(password) || !lowerCase.MatchString(password) {
+		return errors.ErrInvalidPassword
+	}
+	if !digit.MatchString(password) {
+		return errors.ErrInvalidPassword
+	}
+	return nil
+}
+
+func ValidateEmail(email string) error {
+	emailReg := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	if !emailReg.MatchString(email) {
+		return errors.ErrInvalidEmailFormat
+	}
+	return nil
 }
