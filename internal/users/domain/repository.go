373d12e39fc81d644f9c04a42087041eec221a3b
usmanov0 <azikusmanov882@gmail.com>
2023-12-28 15:03:46 +0500

    adapter implementations

--- internal/users/domain/repository.go
+++ internal/users/domain/repository.go
@@ -4,4 +4,7 @@ type UserRepository interface {
 	Save(user *User) (int, error)
 	FindById(id int) (*User, error)
 	ExistsByMail(email string) (bool, error)
+	Search(criteria string) ([]*User, error)
+	Update(userID int, user *User) error
+	Delete(id int) error
 }
