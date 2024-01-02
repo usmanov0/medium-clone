e6497f88973840bd62bc53a5cffc224c0441d793
usmanov0 <azikusmanov882@gmail.com>
2024-01-02 20:34:33 +0500

    some errors

--- internal/users/errors/errors.go
+++ internal/users/errors/errors.go
@@ -12,6 +12,10 @@ const (
 	ErrScanRows            = Err("Failed to scan rows")
 	ErrFailedDeleteAccount = Err("Failed to delete user account")
 	ErrDeletingUser        = Err("Failed to delete user")
+	ErrArticleTitleEmpty   = Err("article title can't be empty")
+	ErrArticleBodyEmpty    = Err("article body can't be empty")
+	ErrArticleExceedTitle  = Err("article title exceeds maximum length")
+	ErrArticleExceedBody   = Err("article body exceeds maximum length")
 )
 
 type Err string
