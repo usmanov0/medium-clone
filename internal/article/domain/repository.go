09dfcd4a418d98aebd794807039ebc723fc04d23
usmanov0 <azikusmanov882@gmail.com>
2024-01-02 20:34:17 +0500

    article adapters with implementation

---
+++ internal/article/domain/repository.go
@@ -0,0 +1,11 @@
+package domain
+
+type ArticleRepository interface {
+	Save(article *Article) (int, error)
+	FindById(id int) (*Article, error)
+	FindByAuthor(authorID int) ([]*Article, error)
+	FindByCategory(categoryID int) ([]*Article, error)
+	FindPublishedArticles() ([]*Article, error)
+	Update(article *Article) error
+	Delete(id int) error
+}
